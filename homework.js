85e1268ce07198fa035bbcdd858096c41e94dce5
Yara Aguilar <yaraaguilar987@gmail.com>
2021-03-19 19:57:57 -0400

    Finally fixed a few things after seeing gc post

--- homework.js
+++ homework.js
@@ -1,21 +1,35 @@
-sampleRate            : 16000  // audio sample rate
-channels              : 1      // number of channels
-threshold             : 0.5    // silence threshold (rec only)
-endOnSilence          : false  // automatically end on silence (if supported)
-thresholdStart        : null   // silence threshold to start recording, overrides threshold (rec only)
-thresholdEnd          : null   // silence threshold to end recording, overrides threshold (rec only)
-silence               : '1.0'  // seconds of silence before ending
-recorder              : 'sox'  // Defaults to 'sox'
-device                : null   // recording device (e.g.: 'plughw:1')
-audioType             : 'mp3'  // audio type to record
-
-const recorder = require('node-record-lpcm16')
-const fs = require('fs')
+const screenshot = require('screenshot-desktop')
  
-const file = fs.createWriteStream('test.mp3', { encoding: 'binary' })
- 
-recorder.record({
-  sampleRate: 44100
+screenshot().then((img) => {
+  // img: Buffer filled with jpg goodness
+  // ...
+}).catch((err) => {
+  // ...
 })
-.stream()
-.pipe(file)
+ 
+screenshot({format: 'png'}).then((img) => {
+  // img: Buffer filled with png goodness
+  // ...
+}).catch((err) => {
+  // ...
+})
+
+screenshot.listDisplays().then((displays) => {
+  // displays: [{ id, name }, { id, name }]
+  screenshot({ screen: displays[displays.length - 1].id })
+    .then((img) => {
+      // img: Buffer of screenshot of the last display
+    });
+})
+
+screenshot.all().then((imgs) => {
+  // imgs: an array of Buffers, one for each screen
+})
+
+screenshot({ filename: 'shot.jpg' }).then((imgPath) => {
+  // imgPath: absolute path to screenshot
+  // created in current working directory named shot.png
+});
+ 
+// absolute paths work too. so do pngs
+screenshot({ filename: '/Users/brian/Desktop/demo.png' })
